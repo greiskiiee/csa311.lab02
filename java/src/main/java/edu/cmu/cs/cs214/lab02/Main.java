1fe342ee59f0703476d825bce9837e31db3c01d3
Chimegerdene Drl <Lenovo@DESKTOP-DG7QGI5.(none)>
2025-02-13 21:53:52 +0800

    encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,24 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
+
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(0.4);
+        Shape square = new Square(3);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer1 = new Renderer(rectangle);
+        Renderer renderer2 = new Renderer(circle);
+        Renderer renderer3 = new Renderer(square);
 
-        renderer.draw();
+        renderer1.draw();
+        renderer2.draw();
+        renderer3.draw();
+
     }
 }
